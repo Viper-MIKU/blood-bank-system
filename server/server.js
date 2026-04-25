84c7dd3018eeeff0beb7ab76e1fa21322a80fcbc
Vinayak <vinayaksuryavanshi37@gmail.com>
2026-04-25 11:31:47 +0530

    Fix MongoDB URI connection string

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI || process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((error) => console.log("DB ERROR:", error));
 
